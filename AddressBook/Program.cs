2027dca89178a4aa57898ae85f34c3c5b44443ca
Rithika L <rithika.l@tvsnext.com>
2023-07-17 15:21:34 +0530

    [Rithika] Add : Implemented

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -5,12 +5,28 @@ namespace AddressBook
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to AddressBook");
+
             AddressBook addressbook = new AddressBook();
             addressbook.CreateContact();
+            Console.WriteLine("Enter the option to proceed\n 1.Create Contact\n 2.Edit Contact\n");
+            bool flag = true;
+            while (flag)
+            {
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        addressbook.CreateContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter the name to edit contact details");
+                        string input = Console.ReadLine();
+                        addressbook.EditContact(input);
+                        break;
 
+                }
+            }
         }
-
     }
     
 }
