91c4e7776696fb7c409ecf4402fd5c12e5d709dd
Rithika L <rithika.l@tvsnext.com>
2023-07-19 17:36:49 +0530

    [Rithika] Add : Implemented

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -7,8 +7,7 @@ namespace AddressBook
         {
 
             AddressBook addressbook = new AddressBook();
-            addressbook.CreateContact();
-            Console.WriteLine("Enter the option to proceed\n 1.Create Contact\n 2.Edit Contact\n");
+            Console.WriteLine("Enter the option to proceed\n 1.Create Contact\n 2.Add to Dictionary\n 3.Edit Contact\n 4.Display Contact\n 5.Exit ");
             bool flag = true;
             while (flag)
             {
@@ -19,14 +18,12 @@ namespace AddressBook
                         addressbook.CreateContact();
                         break;
                     case 2:
-                        Console.WriteLine("Enter the name to edit contact details");
-                        string input = Console.ReadLine();
-                        addressbook.EditContact(input);
+                        addressbook.AddAddressBookToDictionary();
                         break;
                     case 3:
                         Console.WriteLine("Enter the name of contact details to be deleted");
                         string name = Console.ReadLine();
-                        addressbook.DeleteContact(name);
+                        addressbook.EditContact(name);
                         break;
                     case 4:
                         addressbook.Display();
